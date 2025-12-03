55b4e58540e7c20be42f4621d3e9b32eaf39519a
Clay Eldredge <clay.eldredge@gmail.com>
2025-12-02 21:37:37 -0700

    Remove profile page because it's not important or needed for the main functionality of the app

--- src/profile/profile.jsx
+++ src/profile/profile.jsx
@@ -7,17 +7,7 @@ export function Profile() {
 
     return (
       <main>
-            <div className="profile-card-div">
-                <img src="Mario.png" style={{ width: "75px", height: "75px" }} alt="Mario" />
-                <span>{ username }</span>
-            </div>
-            <div className="profile-info-div">
-                <p>Mains: Mario, Luigi</p>
-                <p>Win Rate: 50%</p>
-            </div>
-            <div className="profile-feed-div">
-                <p>Recent Posts go here</p>
-            </div>
+            Coming soon...
         </main>
     );
 }
